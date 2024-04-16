f22e277f157553556b6ae2447feceb2aa9efbe94
chiraggandhi123 <chiraggandhi70726@gmail.com>
2024-04-17 02:03:07 +0530

    fix: fixes in code and refactor

---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import "./index.css";
+const Navbar = () => {
+  
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <button className="navbar-link  active" data-nav-link="">
+            About
+          </button>
+        </li>
+        <li className="navbar-item">
+          <button className="navbar-link" data-nav-link="">
+            Resume
+          </button>
+        </li>
+        <li className="navbar-item">
+          <button className="navbar-link" data-nav-link="">
+            Portfolio
+          </button>
+        </li>
+        <li className="navbar-item">
+          <button className="navbar-link" data-nav-link="">
+            Blog
+          </button>
+        </li>
+        <li className="navbar-item">
+          <button className="navbar-link" data-nav-link="">
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
